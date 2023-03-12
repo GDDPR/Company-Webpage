b6e95f63696f5862119fa78d17d6301cd355afca
GLDDR <kevinzhengofficial@gmail.com>
2023-03-12 06:38:21 -0400

    commit2

---
+++ main.js
@@ -0,0 +1,12 @@
+let aa = document.getElementById("Btn1");
+aa.addEventListener("mouseover", function(e) {
+    e.style = "background-color: white";
+});
+let bb = document.getElementById("Btn2");
+bb.addEventListener("mouseover", function(e) {
+    e.style = "color: white";
+});
+let cc = document.getElementById("Btn3");
+cc.addEventListener("mouseover", function(e) {
+    e.style = "color: white";
+});
